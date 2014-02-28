95d08782c30ba3c0f436aace01fc110c837d65ba
Andiry Xu <jix024@cs.ucsd.edu>
2014-02-27 21:14:16 -0800

    Add create-queries.py. Note 1M queries are too large. Only create part of them one time.

---
+++ create-queries.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#coding=utf-8
+
+import time
+import os
+import sys
+
+def main():
+	filename = "/root/test/converted1MQuery.txt"
+	f = open(filename, 'r')
+	id1 = 0
+	for line in f:
+		new_file = "./queries/" + str(id1) +".txt"
+		print new_file
+		f1 = open(new_file, 'w')
+		f1.write(line)
+		file.close(f1)
+		id1 = id1 + 1
+		if id1 > 100: break
+	file.close(f)
+
+main()
